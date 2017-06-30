f4899d94a834f4d09bd11a852e41d5e8181c0010
Jian Hua <jh3389@columbia.edu>
2017-06-30 11:53:40 -0400

    refactoring

--- src/main/java/edu/columbia/cuitei/spjdbc/FooBar.java
+++ src/main/java/edu/columbia/cuitei/spjdbc/FooBar.java
@@ -2,6 +2,8 @@ package edu.columbia.cuitei.spjdbc;
 
 import java.sql.Timestamp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FooBar {
 
+    private static final Logger log = LoggerFactory.getLogger(FooBar.class);
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -17,8 +20,12 @@ public class FooBar {
     }
 
     public Timestamp now() {
+        foome();
         return jdbcTemplate.queryForObject("select now()", Timestamp.class);
     }
 
-
+    public void foome() {
+        // -Dmy.active=local -Dmy.size=big
+        log.info("p1={}, p2={}", System.getProperty("my.active"), System.getProperty("my.size"));
+    }
 }
